bb77d8b963063efb8928e2b541aaa5b455099b2b
hranikBs23 <Habibur.Rahman@brainstation-23.com>
2023-05-21 12:59:36 +0600

    initial setup

---
+++ Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Persistence.Contracts
+{
+	internal interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+	{
+		
+
+	}
+}
---
+++ Application/Profiles/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+	public class MappingProfile: Profile	
+
+	{
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
+    }
+}
